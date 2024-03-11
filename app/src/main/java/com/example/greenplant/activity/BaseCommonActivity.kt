ef730659dfb213512c402fb9beed2fa8f2190767
ChloeMint <959974675@qq.com>
2024-03-11 20:38:36 +0800

    2024.3.11

--- app/src/main/java/com/example/greenplant/activity/BaseCommonActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.greenplant.activity
-
-open class BaseCommonActivity : BaseActivity() {
-}
--- app/src/main/java/com/example/greenplant/util/SuperProgressUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.example.greenplant.util
-
-object SuperProgressUtil {
-    fun killApp(){
-        android.os.Process.killProcess(android.os.Process.myPid())
-    }
-}
