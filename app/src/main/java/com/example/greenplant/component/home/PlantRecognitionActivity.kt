9e8c7ddc8803af93f5b824e6359436f5c553f6c5
ChloeMint <959974675@qq.com>
2024-03-26 10:08:29 +0800

    try to finish taking photos and rotate the photos

--- app/src/main/java/com/example/greenplant/component/home/PlantRecognitionActivity.kt
+++ app/src/main/java/com/example/greenplant/component/home/PlantRecognitionActivity.kt
@@ -1,23 +1,39 @@
 package com.example.greenplant.component.home
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.greenplant.activity.BaseViewModelActivity
 import com.example.greenplant.databinding.ActivityPlantRecognitionBinding
 import com.example.greenplant.util.SuperUiUtil
 import com.permissionx.guolindev.PermissionX
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
+import java.io.File
 
 class PlantRecognitionActivity : BaseViewModelActivity<ActivityPlantRecognitionBinding>() {
+    val takePhoto = 1
+    private lateinit var imageUri:Uri
+    private lateinit var outputImage:File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        outputImage = File(externalCacheDir, "output_image.jpg")
+        if (outputImage.exists()) {
+            outputImage.delete()
+        }
+        outputImage.createNewFile()
+        imageUri =
+            FileProvider.getUriForFile(this, "com.example.greenplant.fileprovider", outputImage)
     }
 
     override fun initViews() {
@@ -60,25 +76,46 @@ class PlantRecognitionActivity : BaseViewModelActivity<ActivityPlantRecognitionB
         }
     }
 
-    private val captureImageLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == RESULT_OK) {
-            // 处理返回的 Intent 数据
-            val extras = result.data?.extras
-            val imageBitmap = extras?.get("data") as Bitmap?
-            binding.takenPhoto.setImageBitmap(imageBitmap)
-        }
-    }
+
 
     // 启动相机 Intent
     private fun launchCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (cameraIntent.resolveActivity(packageManager) != null) {
-            captureImageLauncher.launch(cameraIntent)
-        }
+        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(intent, takePhoto)
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            takePhoto -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    // 将拍摄的照片显示出来
+                    val bitmap = BitmapFactory.decodeStream(contentResolver.
+                    openInputStream(imageUri))
+                    binding.takenPhoto.setImageBitmap(rotateIfRequired(bitmap))
+                }
+            }
+        }
+    }
+    private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
+        val exif = ExifInterface(outputImage.path)
+        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
+            else -> bitmap
+        }
+    }
+    private fun rotateBitmap(bitmap: Bitmap, degree: Int): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height,
+            matrix, true)
+        bitmap.recycle() // 将不再需要的Bitmap对象回收
+        return rotatedBitmap
+    }
 
 }
