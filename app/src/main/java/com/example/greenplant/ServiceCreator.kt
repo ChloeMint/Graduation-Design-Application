449f9a68e4da63ac50a0c274a37a8b51b162220e
ChloeMint <959974675@qq.com>
2024-04-06 17:27:30 +0800

    developing the publish dongtai

--- app/src/main/java/com/example/greenplant/ServiceCreator.kt
+++ app/src/main/java/com/example/greenplant/ServiceCreator.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ServiceCreator {
-    const val BASE_URL = "http://192.168.0.111:5000"
+    const val BASE_URL = "http://192.168.1.148:5000"
     private var okHttpClient = OkHttpClient.Builder()
         .connectTimeout(5,TimeUnit.SECONDS)
         .addInterceptor(AuthorizationInterceptor())
