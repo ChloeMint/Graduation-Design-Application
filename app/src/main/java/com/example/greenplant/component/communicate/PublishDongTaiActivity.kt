8ab7ea31b1eebcf88993a0800bedf1fcb97d1892
ChloeMint <959974675@qq.com>
2024-04-07 15:21:08 +0800

    developing the publish dongtai,add the video

--- app/src/main/java/com/example/greenplant/component/communicate/PublishDongTaiActivity.kt
+++ app/src/main/java/com/example/greenplant/component/communicate/PublishDongTaiActivity.kt
@@ -2,6 +2,8 @@ package com.example.greenplant.component.communicate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.doOnTextChanged
 import com.example.greenplant.activity.BaseViewModelActivity
 import com.example.greenplant.databinding.ActivityPublishDongtaiBinding
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
@@ -12,5 +14,12 @@ class PublishDongTaiActivity : BaseViewModelActivity<ActivityPublishDongtaiBindi
         super.initViews()
         QMUIStatusBarHelper.translucent(this)
         setCenterToolBar(binding.toolbar, "发布动态")
+    }
+
+    override fun initListener() {
+        super.initListener()
+        binding.content.doOnTextChanged { text, start, before, count ->
+            binding.textCount.text = "${text?.length}" + "/100"
+        }
     }
 }
