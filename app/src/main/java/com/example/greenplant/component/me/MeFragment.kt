56f799842fac9357f8d27de2db86035e5bcec43c
ChloeMint <959974675@qq.com>
2024-04-14 19:43:09 +0800

    develop me module 15%

--- app/src/main/java/com/example/greenplant/component/me/MeFragment.kt
+++ app/src/main/java/com/example/greenplant/component/me/MeFragment.kt
@@ -1,13 +1,19 @@
 package com.example.greenplant.component.me
 
 import android.os.Bundle
+import android.util.Log
 import com.example.greenplant.component.home.HomeFragment
 import com.example.greenplant.databinding.FragmentMeBinding
 import com.example.greenplant.fragment.BaseViewModelFragment
 
 
 class MeFragment : BaseViewModelFragment<FragmentMeBinding> (){
-
+    override fun initDatum() {
+        super.initDatum()
+//        binding.cardView.post {
+//            Log.d("MeFragment_cardView", "${binding.cardView.measuredWidth/requireActivity().resources.displayMetrics.density}")
+//        }
+    }
     companion object{
         fun newInstance(): MeFragment {
             val args = Bundle()
