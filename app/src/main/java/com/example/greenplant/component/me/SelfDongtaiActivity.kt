a4c6a099b565c0be67735c105f91f030a8b23048
ChloeMint <959974675@qq.com>
2024-04-17 15:38:04 +0800

    develop selfDongTai module 40%

--- app/src/main/java/com/example/greenplant/component/me/SelfDongtaiActivity.kt
+++ app/src/main/java/com/example/greenplant/component/me/SelfDongtaiActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.greenplant.R
+import com.example.greenplant.ServiceCreator
 import com.example.greenplant.activity.BaseViewModelActivity
 import com.example.greenplant.databinding.ActivitySelfDongtaiBinding
 import com.example.greenplant.entities.Dongtai
 import com.example.greenplant.viewModel.*
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
+import kotlin.properties.Delegates
 
 class SelfDongtaiActivity : BaseViewModelActivity<ActivitySelfDongtaiBinding>() {
     private val getUserDongtaiViewModel by lazy {
@@ -25,11 +29,17 @@ class SelfDongtaiActivity : BaseViewModelActivity<ActivitySelfDongtaiBinding>() 
         ViewModelProvider(this)[DeleteDongtaiViewModel::class.java]
     }
 
+    private val userInfoViewModel by lazy {
+        ViewModelProvider(this)[UserInfoViewModel::class.java]
+    }
+
     private val dataList = mutableListOf<Dongtai>()
 
     var page = 1
 
     lateinit var adapter: SelfDongTaiAdapter
+
+    private var userId by Delegates.notNull<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +47,28 @@ class SelfDongtaiActivity : BaseViewModelActivity<ActivitySelfDongtaiBinding>() 
             val dongtaiList = it.getOrNull()
             if (dongtaiList != null) {
                 if (dongtaiList.isNotEmpty()){
+                    if (page == 1){
+                        dataList.clear()
+                    }
                     dataList.addAll(dongtaiList)
                     adapter.notifyDataSetChanged()
+                    processRefreshAndLoadMoreStatus(true)
                 }else{
                     page -= 1
+                    processRefreshAndLoadMoreStatus(success = true, noMore = true)
                 }
+            }
+        })
+
+        userInfoViewModel.userInfoLiveData.observe(this, Observer {
+            val user = it.getOrNull()
+            if (user != null){
+                binding.username.text = user.username
+                binding.introduction.text = user.introduction
+                Glide.with(binding.avatar).load(ServiceCreator.BASE_URL + user.avatar).apply {
+                    // 下载error时显示的图片
+                    error(R.drawable.load_failed)
+                }.into(binding.avatar)
             }
         })
     }
@@ -51,18 +78,33 @@ class SelfDongtaiActivity : BaseViewModelActivity<ActivitySelfDongtaiBinding>() 
         QMUIStatusBarHelper.translucent(this)
     }
 
+    override fun initListener() {
+        super.initListener()
+        binding.refreshLayout.setOnRefreshListener {
+            page = 1
+            getUserDongtaiViewModel.setUserId(userId,page)
+        }
+
+        binding.refreshLayout.setOnLoadMoreListener {
+            page+=1
+            getUserDongtaiViewModel.setUserId(userId,page)
+        }
+    }
+
     override fun initDatum() {
         super.initDatum()
-        val userId = intent.getIntExtra("userId",0)
+        userId = intent.getIntExtra("userId",0)
         getUserDongtaiViewModel.setUserId(userId,page)
         adapter = SelfDongTaiAdapter(this, dataList, userId, likeAndCancelViewModel, deleteDongtaiViewModel)
         binding.selfDongtaiRecycleView.adapter = adapter
         binding.selfDongtaiRecycleView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
+        userInfoViewModel.setIsGettingUserInfo()
     }
 
-    fun refreshData(){
-
+    private fun processRefreshAndLoadMoreStatus(success:Boolean, noMore:Boolean = false){
+        binding.refreshLayout.finishRefresh(500,success,false)
+        // 这里的noMoreData是为loadMore方法服务的
+        binding.refreshLayout.finishLoadMore(500,success,noMore)
     }
 
     companion object{
